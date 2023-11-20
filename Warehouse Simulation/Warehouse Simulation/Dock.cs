9782aa02a7cb5f7b76c94928d6169809bc590ef7
Caden Atwell <99625358+CAtwell2@users.noreply.github.com>
2023-11-19 20:51:21 -0500

    Add files via upload

--- Warehouse Simulation/Warehouse Simulation/Dock.cs
+++ Warehouse Simulation/Warehouse Simulation/Dock.cs
@@ -25,30 +25,24 @@ namespace Warehouse_Simulation
         /// Gets the unique identifier of the dock.
         /// </summary>
         public string Id { get; private set; }
-
         // Queue of trucks waiting to be processed at this dock.
         private Queue<Truck> Line;
-
         /// <summary>
         /// Gets the total sales value of all crates unloaded at this dock.
         /// </summary>
         public double TotalSales { get; private set; }
-
         /// <summary>
         /// Gets the total number of crates unloaded at this dock.
         /// </summary>
         public int TotalCrates { get; private set; }
-
         /// <summary>
         /// Gets the total number of trucks processed at this dock.
         /// </summary>
         public int TotalTrucks { get; private set; }
-
         /// <summary>
         /// Gets the total time increments this dock was in use.
         /// </summary>
         public int TimeInUse { get; private set; }
-
         /// <summary>
         /// Gets the total time increments this dock was not in use.
         /// </summary>
@@ -68,7 +62,6 @@ namespace Warehouse_Simulation
             TimeInUse = 0;
             TimeNotInUse = 0;
         }
-
         /// <summary>
         /// Adds a truck to the line of trucks waiting at this dock.
         /// </summary>
@@ -78,7 +71,6 @@ namespace Warehouse_Simulation
             Line.Enqueue(truck);
             TotalTrucks++;
         }
-
         /// <summary>
         /// Checks if there are any trucks waiting in line at this dock.
         /// </summary>
@@ -87,19 +79,60 @@ namespace Warehouse_Simulation
         {
             return Line.Count == 0;
         }
-
+        public Truck SendOff()
+        {
+            return Line.Count > 0 ? Line.Dequeue() : null;
+        }
         /// <summary>
         /// Processes the first truck in line at this dock, unloading a crate if available.
         /// </summary>
         /// <param name="warehouseInstance">Instance of the warehouse containing this dock.</param>
         public void ProcessTruck(Warehouse warehouseInstance)
         {
-            // ... existing implementation ...
+            if (Line.Count > 0)
+            {
+                Truck currentTruck = Line.Peek();
+                Crate crate = currentTruck.Unload();
+                if (crate != null)
+                {
+
+                    string scenario = DetermineUnloadingScenario(currentTruck, warehouseInstance);
+
+
+                    warehouseInstance.LogCrateUnloading(crate, currentTruck, scenario);
+
+                    TotalSales += crate.Price;
+                    TotalCrates++;
+                    TimeInUse++;
+                }
+
+                if (currentTruck.IsEmpty())
+                {
+                    SendOff();
+                }
+            }
+            else
+            {
+                TimeNotInUse++;
+            }
         }
 
         private string DetermineUnloadingScenario(Truck truck, Warehouse warehouseInstance)
         {
-            // ... existing implementation ...
+            if (!truck.IsEmpty())
+            {
+                return "A crate was unloaded, but the truck still has more crates to unload";
+            }
+            else if (Line.Count > 1 || (Line.Count == 1 && warehouseInstance.Entrance.Count > 0))
+            {
+                return "A crate was unloaded, and the truck has no more crates to unload, and another truck is already in the Dock";
+            }
+            else
+            {
+                return "A crate was unloaded, and the truck has no more crates to unload, but another truck is NOT already in the Dock";
+            }
+
         }
     }
 }
+
